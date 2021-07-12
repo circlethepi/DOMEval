7b095c2452b3438d008a20e809c122b15ae3afa7
cford <kcford26@gmail.com>
2021-07-12 18:20:02 -0500

    card/distibutionmanager/hypothesis/Cardset

--- src/main/scala/DistroManager/Hypothesis.scala
+++ src/main/scala/DistroManager/Hypothesis.scala
@@ -5,9 +5,9 @@ package DistroManager
  * July 2021
  *
  * @param card
- * @param outlier
+ * @param outlier # of std devs away from sample mu it is, below a threshold it counts as an outlier
  */
-case class Hypothesis(card : Card, outlier : Option[Double]) {
+case class Hypothesis(card : Card, outlier_score : Double ) {
 
   val mu  : Double = card.get_distro()._1
   val sig : Double = card.get_distro()._2
