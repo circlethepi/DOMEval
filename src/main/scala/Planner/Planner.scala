42b9557e6a23e2347dd4e1e4db32aa76b6124fb5
cford <kcford26@gmail.com>
2021-07-15 16:58:42 -0500

    planner  + some data i suppose

--- src/main/scala/Planner/Planner.scala
+++ src/main/scala/Planner/Planner.scala
@@ -1,6 +1,43 @@
 package Planner
 
+import DistroManager.{Card, Hypothesis}
+
+import scala.collection.mutable.ListBuffer
+
 object Planner {
+
+  //12 episodes per machine
+  val machines = 3
+  val episodespermachine = 12
+  val MAX_EPISODES : Int = machines*episodespermachine
+  val MIN_KINGDOM_SIZE = 2
+
+  def Plan(hypotheses : List[Hypothesis]): List[List[Card]] = {
+
+    //organize from least powerful to most powerful cards
+    //episode with lower (...,4,5,6,...) eval cards
+    //episode with uppper (...,4,5,6,...) eval cards
+
+    val sorted = hypotheses.sortBy(_.mu)
+    val ret = ListBuffer[List[Card]]()
+
+    var currkingdom = MIN_KINGDOM_SIZE
+    for(i <- 0 until MAX_EPISODES/2) {
+      val lowkingdom = ListBuffer[Card]()
+      val highkingdom = ListBuffer[Card]()
+
+      for(s<-0 until currkingdom) {
+        lowkingdom.addOne(sorted(s).card)
+        highkingdom.addOne(sorted.reverse(s).card)
+      }
+
+      ret.addOne(lowkingdom.toList)
+      ret.addOne(highkingdom.toList)
+      currkingdom += 1
+    }
+
+    ret.toList
+  }
 
   //reeeeeEEEEEEEEEEEE
 
